9256221119d313a3fa342c22d3340295b8816791
PabloBrubeck <brubeck@protonmail.com>
2016-04-26 14:03:13 -0500

    angular normalization

--- Visualization/sphericalPlot.m
+++ Visualization/sphericalPlot.m
@@ -1,4 +1,4 @@
-function [h,th,ph] = sphericalPlot(n, m)
+function [h,ph,th] = sphericalPlot(n, m)
 u=2*mod((0:n-1),n/2)/(n/2-1)-1;
 v=2*(0:m-1)/(m-1)-1;
 k=2*(0:n-1)>=n;
@@ -11,19 +11,19 @@ rr(~idx)=vv(~idx);
 if(mod(n,2)==1 && mod(m,2)==1)
     uu((n+1)/2,(m+1)/2)=1;
 end
-th(idx)=pi/4*(vv(idx)./uu(idx));
-th(~idx)=pi/4*(2-uu(~idx)./vv(~idx));
+ph(idx)=pi/4*(vv(idx)./uu(idx));
+ph(~idx)=pi/4*(2-uu(~idx)./vv(~idx));
 
 rr=reshape(rr,size(uu));
-th=reshape(th,size(uu));
+ph=reshape(ph,size(uu));
 
 tt=rr.*sqrt(2-rr.^2);
-xx=tt.*cos(th); 
-yy=tt.*sin(th); 
+xx=tt.*cos(ph); 
+yy=tt.*sin(ph); 
 zz=(1-rr.^2);
 zz(k,:)=-zz(k,:);
 
-ph=acos(sign(zz).*(1-rr.^2));
-th=mod(th+pi*(rr<=0), 2*pi);
+ph=mod(ph+pi*(rr<=0), 2*pi);
+th=acos(sign(zz).*(1-rr.^2));
 h=surf(xx,yy,zz); axis equal;
 end
