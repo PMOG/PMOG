b6894e27d44b791d8642cf28930e60c4f5595345
PabloBrubeck <brubeck@protonmail.com>
2016-05-19 11:30:44 -0500

    Grating Intensity Normalization

--- Holography/grating.m
+++ Holography/grating.m
@@ -6,10 +6,10 @@ function [H] = grating(E, xx, yy, k, th, mode)
 % mode defines whether the intensity is modulated or not
 th=pi/180*th;
 plane=sin(th)*xx+cos(th)*yy;
-phase=angle(E);
-H=mod(phase+k*plane+pi, 2*pi)-pi;
-if mode 
-    H=(E.*conj(E)).*H;
+H=mod(angle(E)+k*plane+pi, 2*pi)-pi;
+if mode
+    EE=E.*conj(E);
+    H=(EE/max(EE(:))).*H;
 end
 H=uint8(255*(H+pi)/(2*pi)); % Normalize to grayscale
 end
